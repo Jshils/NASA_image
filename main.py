4635a22c60ac689502a7ffc953083ce4b24e02d6
jshils <biostim@gmail.com>
2024-03-08 14:48:34 -0600

    Added a input to place older days on web

--- main.py
+++ main.py
@@ -9,34 +9,36 @@ print(now)
 
 st.set_page_config(layout="wide")
 
-while True:
-       url = "https://api.nasa.gov/planetary/apod?" \
-              f"date={now}&" \
-              "api_key=x1I5fdzd7jqpICqhquppDNNzsgekH4dI3DDRYcsp"
+now = st.text_input(label="Enter the date in the format YYYY-MM-DD :", value=now, key="new_date")
+
+url = "https://api.nasa.gov/planetary/apod?" \
+       f"date={now}&" \
+       "api_key=x1I5fdzd7jqpICqhquppDNNzsgekH4dI3DDRYcsp"
 
 
-       # Get image of the day from the NASA library
-       response = requests.get(url)
-       content = response.json()
+# Get image of the day from the NASA library
+response = requests.get(url)
+content = response.json()
 
-       # Get the name of the image url
-       hold = content["url"]
-       explanation = content["explanation"]
+# Get the name of the image url
+hold = content["url"]
+explanation = content["explanation"]
 
-       # Get the binary data of the picture
-       picture = requests.get(hold)
+# Get the binary data of the picture
+picture = requests.get(hold)
 
-       # Open a file to save the image in
-       with open("image.jpg", "wb") as file:
-              file.write(picture.content)
+# Open a file to save the image in
+with open("image.jpg", "wb") as file:
+       file.write(picture.content)
 
-       # Start the web app
-       col1, col2 = st.columns(2)
-       with col1:
-              st.image("image.jpg")
+# Start the web app
+col1, col2 = st.columns(2)
+with col1:
+       st.image("image.jpg")
 
-       with col2:
-              st.info(explanation)
-              st.info(now)
-              now = input("Enter the date in the format YYYY-MM-DD :")
-              print(now)
+with col2:
+       st.info(explanation)
+       st.info(now)
+
+        # now = input("Enter the date in the format YYYY-MM-DD :")
+print(now)
